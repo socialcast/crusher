c768bbfe8948b675c7063c09e1d20b0b36233835
Mitch Williams <mitch@socialcast.com>
2010-10-02 12:25:37 -0700

    Modularized in preparation for gemification

---
+++ lib/crusher/load_generator.rb
@@ -0,0 +1,38 @@
+module Crusher
+  
+  class LoadGenerator
+  
+    def initialize(options)
+      @options = options
+      @log_file = File.open(options[:log_file], 'a') if options[:log_file]
+    
+      # Reseed the pseudorandom number generator with the process's PID file,
+      # as we assume this code will be run in a forked process.
+      Kernel.srand(Process.pid)
+    end
+  
+    def start!
+      log('Booting')
+      prepare
+      while Time.now < @options[:end_time]
+        act!
+      end
+      log('Terminating')
+    end
+  
+    def act!; end
+  
+    def prepare; end
+  
+    def log(message)
+      log_message = [Time.now.strftime('%Y-%m-%d %H:%M:%S'), @options[:process_name], message].compact.join(' ') + "\n"
+      if @log_file
+        @log_file.syswrite(log_message)
+      else
+        puts log_message
+      end
+    end
+  
+  end
+  
+end
---
+++ lib/crusher/web_user.rb
@@ -0,0 +1,94 @@
+gem 'httpclient'
+gem 'crack'
+require 'crack/json'
+require "httpclient"
+
+module Crusher
+
+  class WebUser < LoadGenerator
+  
+    def initialize(uri_root, email, password, options)
+      super(options)
+      @uri_root = uri_root
+      @email = email
+      @password = password
+      @client = HTTPClient.new
+      @messageIds = {}
+    end
+  
+    def prepare
+      login
+      get_messages
+    end
+  
+    def ready?
+      @ready
+    end
+  
+    def act!
+      @ready = false
+      case rand(10)
+      when 0..2  
+        get_messages
+      when 3..6
+        post_message
+      when 7..9
+        post_comment
+      end
+      sleep rand(4)
+      @ready = true
+    end
+  
+    def login
+      request :post, "#{@uri_root}/sessions", {'email' => @email, 'password' => @password} do |result, duration|
+        log "POST loging in as #{@email}:#{@password} #{result} in #{duration}s"
+      end
+    end  
+  
+    def get_messages
+
+      result = request :get, "#{@uri_root}/api/messages.json" do |result, duration|
+        log "GET message list #{result} in #{duration}s"
+      end
+    
+      if result
+        messages = Crack::JSON.parse(result.content)['messages']
+        @messageIds = {}
+        messages.each do |message|
+          @messageIds[message['id'].to_i] = message
+        end
+      end
+    
+    end
+  
+    def post_message
+      request :post, "#{@uri_root}/api/messages.json", {'message[body]' => Util.lipsum([5, rand(50)].max)} do |result, duration|
+        log "POST message #{result} in #{duration}s"
+      end
+    end
+  
+    def post_comment
+      id = @messageIds.keys[rand(@messageIds.keys.size)]
+      request :post, "#{@uri_root}/api/messages/#{id}/comments", {'comment[comment]' => Util.lipsum([5, rand(25)].max)} do |result, duration|
+        log "POST comment #{result} in #{duration}s"
+      end
+    end
+  
+    def request(*args)
+      response = nil
+      start_time = Time.now
+      result = nil
+      begin
+        response = @client.send(*args) 
+        result = 'succeeded'
+      rescue HTTPClient::ReceiveTimeoutError => e
+        result = 'timed out'
+      end
+      duration = Time.now - start_time
+      yield(result, duration)
+      return response
+    end
+    
+  end
+  
+end
