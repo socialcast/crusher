4124e51ce20df0d23db9884710e8b0d611426c0d
Mitch Williams <mitch@socialcast.com>
2010-10-05 16:40:33 -0700

    Moved to a multithreading and multiprocessing solution

--- lib/crusher/load_generator.rb
+++ lib/crusher/load_generator.rb
@@ -8,17 +8,13 @@ module Crusher
         @log_file = File.open(options[:log_file], 'a') if options[:log_file] 
       rescue Errno::ENOENT
         log("Log file #{options[:log_file]} couldn't be opened; logging to STDOUT")
-      end
-    
-      # Reseed the pseudorandom number generator with the process's PID file,
-      # as we assume this code will be run in a forked process.
-      Kernel.srand(Process.pid)
+      end    
     end
   
     def start!
       log('Booting')
       prepare
-      while Time.now < @options[:end_time]
+      while running?
         act!
       end
       log('Terminating')
@@ -27,14 +23,19 @@ module Crusher
     def act!; end
   
     def prepare; end
+    
+    def running?
+      Time.now < @options[:end_time]
+    end
   
     def log(message)
-      log_message = [Time.now.strftime('%Y-%m-%d %H:%M:%S'), @options[:process_name], message].compact.join(' ') + "\n"
+      log_message = Time.now.strftime('%Y-%m-%d %H:%M:%S') + ' ' + @options[:process_name] + ' ' +  message + "\n"
       if @log_file
         @log_file.syswrite(log_message)
       else
         puts log_message
       end
+      nil
     end
   
   end
