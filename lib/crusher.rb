d9a7a79197f181def242b738424970153b6f497c
Mitch Williams <mitch@socialcast.com>
2010-10-12 19:17:30 -0700

    Removed webuser file

--- lib/crusher.rb
+++ lib/crusher.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'crusher/load_generator'
 require 'crusher/cookie'
 require 'crusher/http_load_generator'
-require 'crusher/web_user'
 require 'crusher/util'
 require 'crusher/configurator'
 
